38a47e764d6d44de33aaefeed354c86ff0568ad9
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-09-08 21:59:20 +0300

    http error helper

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,5 @@
 const { Product } = require("../models/product");
+const { HttpError } = require("../helpers");
 
 const getAllProducts = async (req, res) => {
   const result = await Product.find({});
@@ -8,6 +9,10 @@ const getAllProducts = async (req, res) => {
 const getOneProduct = async (req, res) => {
   const { id } = req.params;
   const result = await Product.findById(id);
+
+  if (!result) {
+    throw HttpError(404, `Product with "${id}" not found`);
+  }
 
   res.json(result);
 };
