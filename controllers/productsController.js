ae8723efec1d6b7a717a6164eb8bc520b920b3d4
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-09-11 18:48:19 +0300

    recently viewed

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,6 +2,8 @@ const { Product } = require("../models/product");
 const { HttpError } = require("../helpers");
 const { ControllerWrap } = require("../decorators/controllerWrap");
 
+let recentlyViewed = [];
+
 const getAllProducts = async (req, res) => {
   const { page = 1, limit = 5 } = req.query;
   const skip = (page - 1) * limit;
@@ -17,7 +19,15 @@ const getOneProduct = async (req, res) => {
     throw HttpError(404, `Product with "${id}" not found`);
   }
 
-  res.json(result);
+  if (
+    !recentlyViewed.some((viewedProduct) =>
+      viewedProduct._id.equals(result._id)
+    )
+  ) {
+    recentlyViewed.push(result);
+  }
+
+  res.json({ result, message: "Product added to recently viewed" });
 };
 
 const updateFavorite = async (req, res) => {
@@ -33,8 +43,13 @@ const updateFavorite = async (req, res) => {
   res.json(result);
 };
 
+const getRevetlyViewed = async (req, res) => {
+  res.json(recentlyViewed);
+};
+
 module.exports = {
   getAllProducts: ControllerWrap(getAllProducts),
   getOneProduct: ControllerWrap(getOneProduct),
   updateFavorite: ControllerWrap(updateFavorite),
+  getRevetlyViewed: ControllerWrap(getRevetlyViewed),
 };
--- routes/products.js
+++ routes/products.js
@@ -5,11 +5,13 @@ const {
   getAllProducts,
   getOneProduct,
   updateFavorite,
+  getRevetlyViewed,
 } = require("../controllers/productsController");
 
 const { isValidId } = require("../middlewares/isValidId");
 
 router.get("/", getAllProducts);
+router.get("/", getRevetlyViewed);
 router.get("/:id", isValidId, getOneProduct);
 router.patch("/:id", isValidId, updateFavorite);
 
