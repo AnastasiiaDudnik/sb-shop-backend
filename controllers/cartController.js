de373b85d94dbed5e43e4c8f596a2ab43c0aa575
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-09-18 21:33:13 +0300

    remove from cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -22,12 +22,28 @@ const addToShoppingCart = async (req, res) => {
     throw HttpError(404, `Product with ${id} not found`);
   }
 
-  console.log(product);
   const result = await Cart.create({ items: product });
   res.status(201).json(result);
+};
+
+const removeFromCart = async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+
+  if (!product) {
+    throw HttpError(404, `Product with ${id} not found`);
+  }
+
+  const result = await Cart.findByIdAndRemove(product._id);
+
+  res.json({
+    message: "product deleted",
+    result,
+  });
 };
 
 module.exports = {
   getShoppingCart: controllerWrap(getShoppingCart),
   addToShoppingCart: controllerWrap(addToShoppingCart),
+  removeFromCart: controllerWrap(removeFromCart),
 };
--- routes/cart.js
+++ routes/cart.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   getShoppingCart,
   addToShoppingCart,
+  removeFromCart,
 } = require("../controllers/cartController");
 
 router.get("/", getShoppingCart);
 router.post("/:id", addToShoppingCart);
+router.delete("/:id", removeFromCart);
 
 module.exports = router;
