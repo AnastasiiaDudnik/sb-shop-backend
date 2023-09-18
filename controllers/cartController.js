55018f7fb7287576e2266034fd9b97ddcc40228e
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-09-18 21:34:33 +0300

    get cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,14 +4,8 @@ const { HttpError } = require("../helpers");
 const { controllerWrap } = require("../decorators/controllerWrap");
 
 const getShoppingCart = async (req, res) => {
-  // const { sessionId } = req.sessions;
-  // const cartItems = await Cart.find({ sessionId });
-  // let totalPrice = 0;
-  // for (const item of cartItems) {
-  //   totalPrice += item.price * item.quantity;
-  // }
-  // res.json({ cartItems, totalPrice });
-  // res.json(result);
+  const result = await Cart.find({});
+  res.json(result);
 };
 
 const addToShoppingCart = async (req, res) => {
