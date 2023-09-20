bf76b95554395d05bae6806d8d392249ad121161
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-09-20 22:02:49 +0300

    set sessions

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3001 } = process.env;
+const { DB_HOST, PORT = 3001, SESSION_SECRET_KEY } = process.env;
+
+app.use(
+  session({
+    secret: SESSION_SECRET_KEY, // Replace with a strong secret key
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 mongoose
   .connect(DB_HOST)
