d4268900d8afd72c2d5076c4924b7baaed90d146
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-10-10 21:03:10 +0300

    try to set cookie

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const session = require("express-session");
-// const { nanoid } = require("nanoid");
+const cookieParser = require("cookie-parser");
+const { nanoid } = require("nanoid");
 require("dotenv").config();
 
 const productsRouter = require("./routes/products");
@@ -11,17 +12,22 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+const { SESSION_SECRET_KEY } = process.env;
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-const { SESSION_SECRET_KEY } = process.env;
+app.use(cookieParser());
 
 app.use(
   session({
     secret: SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: true,
+    // name: "my-session-cookie", // Change the cookie name
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
   })
 );
 
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,20 +2,28 @@ const Product = require("../models/product");
 const { HttpError } = require("../helpers");
 const { controllerWrap } = require("../decorators/controllerWrap");
 
-// let recentlyViewed = [];
-
 const getAllProducts = async (req, res) => {
+  const { id: sessionId } = req.session;
+  res.cookie("guest", sessionId, {
+    domain: "https://anastasiiadudnik.github.io/shop-test/",
+  });
+  // console.log(sessionId);
+
   const { page = 1, limit = 5 } = req.query;
   const skip = (page - 1) * limit;
 
   const result = await Product.find({}, null, { skip, limit });
 
   res.json(result);
+  console.log(res);
 };
 
 const getOneProduct = async (req, res) => {
   const { id } = req.params;
-  // const { session } = req;
+  // const { id: sessionId } = req.session;
+
+  // const { guest } = req.cookies;
+  // console.log(guest);
 
   const result = await Product.findById(id);
 
@@ -23,19 +31,21 @@ const getOneProduct = async (req, res) => {
     throw HttpError(404, `Product with "${id}" not found`);
   }
 
-  // const recentlyViewed = req.session.recentlyViewed || [];
+  // res.cookie("guestID", sessionId, { maxAge: 30 * 24 * 60 * 60 * 1000 }); //store for 30 days
+
+  const recentlyViewed = req.session.recentlyViewed || [];
 
   // Check if the product is already in the recently viewed list
-  // const index = recentlyViewed.findIndex(
-  //   (product) => product._id.toString() === id
-  // );
+  const index = recentlyViewed.findIndex(
+    (product) => product._id.toString() === id
+  );
 
-  // // If the product is not in the list, add it
-  // if (index === -1) {
-  //   recentlyViewed.push(result);
-  // }
+  //  If the product is not in the list, add it
+  if (index === -1) {
+    recentlyViewed.push(result);
+  }
 
-  res.json({ result, session });
+  res.json({ result, recentlyViewed });
 };
 
 const updateFavorite = async (req, res) => {
